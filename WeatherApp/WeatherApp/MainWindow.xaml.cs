7bdc8b507bae994244824c8ce1599b0ac3a3ab45
szyfir <szyfir+szymon.firlinger@gmail.com>
2018-03-07 23:47:22 +0100

    dodanie metody GetWeather, pobierajacej z weather info api informacje o pogodzie w gdansku w formacie json

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,19 @@ namespace WeatherApp
         public MainWindow()
         {
             InitializeComponent();
+            GetWeather();
+        }
+        public void GetWeather()
+        {
+            WebClient client = new WebClient();
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Gdansk&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0"; //json format (weather info api)
+            var json = client.DownloadString(url);
+
+            var results = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+            WeatherInfo.root Output = results;
+            //wyswietlanie informacji
+            textBox1.Text = string.Format("{0} \u00B0" + "C", Output.main.temp-273.15);    //przeliczenie temp
+
         }
     
     }
