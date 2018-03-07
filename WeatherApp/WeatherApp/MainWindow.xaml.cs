54fb3cb2be521cdbf9aa0bc4ee2110795d5f1d0d
szyfir <szyfir+szymon.firlinger@gmail.com>
2018-03-08 00:11:34 +0100

    proba odczytu

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -26,19 +26,33 @@ namespace WeatherApp
         {
             InitializeComponent();
             GetWeather();
+            GetForecast();
         }
         public void GetWeather()
         {
             WebClient client = new WebClient();
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=Gdansk&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0"; //json format (weather info api)
+            string url = @"http://api.openweathermap.org/data/2.5/weather?q=Gdansk&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0"; //json format (weather info api)
             var json = client.DownloadString(url);
 
             var results = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
             WeatherInfo.root Output = results;
             //wyswietlanie informacji
-            textBox1.Text = string.Format("{0} \u00B0" + "C", Output.main.temp-273.15);    //przeliczenie temp
+            textBox1.Text = string.Format("{0} \u00B0" + "C", Output.main.temp - 273.15);    //przeliczenie temp
+
+
 
         }
-    
+        public void GetForecast()
+        {
+            WebClient client = new WebClient();
+            string url = @"http://api.openweathermap.org/data/2.5/forecast?q=Gdansk&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0";
+            var json = client.DownloadString(url);
+
+            var results = JsonConvert.DeserializeObject<Forecast.RootObject>(json);
+            Forecast.RootObject Output = results;
+            textBox2.Text = string.Format("{0:N1} \u00B0" + "C", Output.list[1].main.temp - 273.15);
+            textBox3.Text = string.Format("{0}", Output.list[1].dt_txt);
+
+        }
     }
 }
