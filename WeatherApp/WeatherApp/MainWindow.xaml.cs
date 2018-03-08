3b3b4baf288725ecd699e32d27a56830f7b335b8
szyfir <szyfir+szymon.firlinger@gmail.com>
2018-03-08 10:40:18 +0100

    rozwijanie szaty graficznej

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -22,22 +22,34 @@ namespace WeatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
-            GetWeather();
+            GetWeather("Gdańsk");
             GetForecast();
         }
-        public void GetWeather()
+        public void GetWeather(string city)
         {
-            WebClient client = new WebClient();
-            string url = @"http://api.openweathermap.org/data/2.5/weather?q=Gdansk&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0"; //json format (weather info api)
-            var json = client.DownloadString(url);
 
-            var results = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
-            WeatherInfo.root Output = results;
-            //wyswietlanie informacji
-            textBox1.Text = string.Format("{0} \u00B0" + "C", Output.main.temp - 273.15);    //przeliczenie temp
+            try
+            {
+                WebClient client = new WebClient();
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&APPID=42c1d2a0b9b958f4fbb51e0edc9896a0", city); //json format (weather info api)
+                var json = client.DownloadString(url);
+
+                var results = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                WeatherInfo.root Output = results;
+                //wyswietlanie informacji
+                textBox1.Text = string.Format("{0:N1} \u00B0" + "C", Output.main.temp - 273.15);    //przeliczenie temp
+                lLocation.Content = city.ToUpper();
+            }
+                
+            catch(Exception)
+            {
+                MessageBox.Show("Nie znaleziono nazwy miejscowości");
+            }
+           
 
 
 
@@ -53,6 +65,27 @@ namespace WeatherApp
             textBox2.Text = string.Format("{0:N1} \u00B0" + "C", Output.list[1].main.temp - 273.15);
             textBox3.Text = string.Format("{0}", Output.list[1].dt_txt);
 
+        }
+
+        private void tbSearch_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbSearch.Text = null;
+
+        }
+
+
+
+       
+
+        private void bSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(tbSearch.Text)==false)
+            {
+
+                GetWeather(tbSearch.Text.ToString());
+                
+            }
+          
         }
     }
 }
