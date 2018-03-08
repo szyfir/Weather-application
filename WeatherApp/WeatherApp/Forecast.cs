1a873e853b93ef01bbc0ff181ae708367e01d1a0
szyfir <szyfir+szymon.firlinger@gmail.com>
2018-03-08 10:42:38 +0100

    dodanie niezbednych packages

---
+++ WeatherApp/WeatherApp/Forecast.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class Forecast
+    {
+        public class Main
+        {
+            public double temp { get; set; }
+
+            public double pressure { get; set; }
+
+            public int humidity { get; set; }
+
+        }
+
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+
+        }
+
+
+
+        public class Wind
+        {
+            public double speed { get; set; }
+
+        }
+
+
+
+        public class List
+        {
+            public int dt { get; set; }
+            public Main main { get; set; }
+            public List<Weather> weather { get; set; }
+
+            public Wind wind { get; set; }
+
+
+            public string dt_txt { get; set; }
+        }
+
+        public class Coord
+        {
+            public double lat { get; set; }
+            public double lon { get; set; }
+        }
+
+        public class City
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public Coord coord { get; set; }
+            public string country { get; set; }
+
+        }
+
+        public class RootObject
+        {
+            public string cod { get; set; }
+            public double message { get; set; }
+            public int cnt { get; set; }
+            public List<List> list { get; set; }
+            public City city { get; set; }
+        }
+    }
+}
